fcd40d914bd5815dc33cd1365730aa273f95cf13
Terranie Sims <terranie67@gmail.com>
2020-04-29 15:35:00 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ def find_min_in_nested_arrays(src)
     element_index = 0 
     min_in_nested_array = ""
     while element_index < src[row_index].count do 
-      if src[row_index][element_index].length <  min_in_nested_array.length 
+      if src[row_index][element_index].size <  min_in_nested_array.size  
         min_in_nested_array = src[row_index][element_index]
       end
       element_index += 1 
